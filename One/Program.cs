6391040eafd3757ab614df23de16bd22afcd554b
Michaail <galeo09@gmail.com>
2017-04-04 21:07:55 +0200

    Add project files.

---
+++ One/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Tytuł: Pierwszy projekt w Solucji Sol ćwiczenie 2.2 IRunnable
+Opis: 
+Autor: Michał Kłos
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("One");
+
+            Console.ReadLine();
+        }
+    }
+}
